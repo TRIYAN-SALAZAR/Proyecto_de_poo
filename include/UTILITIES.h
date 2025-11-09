b2042e03a6522b52a593f94bfdc5218947c96926
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-11-09 10:30:42 -0600

    Move decorative comment to the end of the file and add ASCII art for enhanced visual representation

--- include/UTILITIES.h
+++ include/UTILITIES.h
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <cstdio>
 
+// (Nota: comentario decorativo movido al final del archivo)
+
 #ifdef _WIN32
 #include <conio.h>
 #else
@@ -70,5 +72,54 @@ inline std::string readPassword()
     std::cout << std::endl;
     return pwd;
 }
+
+/*
+                                                                                                                                                                                           
+                                                                         dddddddd                                                                                                      
+        CCCCCCCCCCCCC                                                    d::::::d  iiii          tttt                                                                                  
+     CCC::::::::::::C                                                    d::::::d i::::i      ttt:::t                                                                                  
+   CC:::::::::::::::C                                                    d::::::d  iiii       t:::::t                                                                                  
+  C:::::CCCCCCCC::::C                                                    d:::::d              t:::::t                                                                                  
+ C:::::C       CCCCCCrrrrr   rrrrrrrrr       eeeeeeeeeeee        ddddddddd:::::d iiiiiiittttttt:::::ttttttt       ooooooooooo       ssssssssss                    aaaaaaaaaaaaa        
+C:::::C              r::::rrr:::::::::r    ee::::::::::::ee    dd::::::::::::::d i:::::it:::::::::::::::::t     oo:::::::::::oo   ss::::::::::s                   a::::::::::::a       
+C:::::C              r:::::::::::::::::r  e::::::eeeee:::::ee d::::::::::::::::d  i::::it:::::::::::::::::t    o:::::::::::::::oss:::::::::::::s                  aaaaaaaaa:::::a      
+C:::::C              rr::::::rrrrr::::::re::::::e     e:::::ed:::::::ddddd:::::d  i::::itttttt:::::::tttttt    o:::::ooooo:::::os::::::ssss:::::s                          a::::a      
+C:::::C               r:::::r     r:::::re:::::::eeeee::::::ed::::::d    d:::::d  i::::i      t:::::t          o::::o     o::::o s:::::s  ssssss                    aaaaaaa:::::a      
+C:::::C               r:::::r     rrrrrrre:::::::::::::::::e d:::::d     d:::::d  i::::i      t:::::t          o::::o     o::::o   s::::::s                       aa::::::::::::a      
+C:::::C               r:::::r            e::::::eeeeeeeeeee  d:::::d     d:::::d  i::::i      t:::::t          o::::o     o::::o      s::::::s                   a::::aaaa::::::a      
+ C:::::C       CCCCCC r:::::r            e:::::::e           d:::::d     d:::::d  i::::i      t:::::t    tttttto::::o     o::::ossssss   s:::::s                a::::a    a:::::a      
+  C:::::CCCCCCCC::::C r:::::r            e::::::::e          d::::::ddddd::::::ddi::::::i     t::::::tttt:::::to:::::ooooo:::::os:::::ssss::::::s               a::::a    a:::::a      
+   CC:::::::::::::::C r:::::r             e::::::::eeeeeeee   d:::::::::::::::::di::::::i     tt::::::::::::::to:::::::::::::::os::::::::::::::s                a:::::aaaa::::::a      
+     CCC::::::::::::C r:::::r              ee:::::::::::::e    d:::::::::ddd::::di::::::i       tt:::::::::::tt oo:::::::::::oo  s:::::::::::ss                  a::::::::::aa:::a     
+        CCCCCCCCCCCCC rrrrrrr                eeeeeeeeeeeeee     ddddddddd   dddddiiiiiiii         ttttttttttt     ooooooooooo     sssssssssss                     aaaaaaaaaa  aaaa     
+                                                                                                                                                                                       
+                                                                                                                                                                                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                            CCCCCCCCCCCCChhhhhhh                                       tttt                 GGGGGGGGGGGGGPPPPPPPPPPPPPPPPP   TTTTTTTTTTTTTTTTTTTTTTT                   
+                         CCC::::::::::::Ch:::::h                                    ttt:::t              GGG::::::::::::GP::::::::::::::::P  T:::::::::::::::::::::T                   
+                       CC:::::::::::::::Ch:::::h                                    t:::::t            GG:::::::::::::::GP::::::PPPPPP:::::P T:::::::::::::::::::::T                   
+                      C:::::CCCCCCCC::::Ch:::::h                                    t:::::t           G:::::GGGGGGGG::::GPP:::::P     P:::::PT:::::TT:::::::TT:::::T                   
+                     C:::::C       CCCCCC h::::h hhhhh         aaaaaaaaaaaaa  ttttttt:::::ttttttt    G:::::G       GGGGGG  P::::P     P:::::PTTTTTT  T:::::T  TTTTTT                   
+                    C:::::C               h::::hh:::::hhh      a::::::::::::a t:::::::::::::::::t   G:::::G                P::::P     P:::::P        T:::::T                           
+                    C:::::C               h::::::::::::::hh    aaaaaaaaa:::::at:::::::::::::::::t   G:::::G                P::::PPPPPP:::::P         T:::::T                           
+                    C:::::C               h:::::::hhh::::::h            a::::atttttt:::::::tttttt   G:::::G    GGGGGGGGGG  P:::::::::::::PP          T:::::T                           
+                    C:::::C               h::::::h   h::::::h    aaaaaaa:::::a      t:::::t         G:::::G    G::::::::G  P::::PPPPPPPPP            T:::::T                           
+                    C:::::C               h:::::h     h:::::h  aa::::::::::::a      t:::::t         G:::::G    GGGGG::::G  P::::P                    T:::::T                           
+                    C:::::C               h:::::h     h:::::h a::::aaaa::::::a      t:::::t         G:::::G        G::::G  P::::P                    T:::::T                           
+                     C:::::C       CCCCCC h:::::h     h:::::ha::::a    a:::::a      t:::::t    ttttttG:::::G       G::::G  P::::P                    T:::::T                           
+                      C:::::CCCCCCCC::::C h:::::h     h:::::ha::::a    a:::::a      t::::::tttt:::::t G:::::GGGGGGGG::::GPP::::::PP                TT:::::::TT                         
+                       CC:::::::::::::::C h:::::h     h:::::ha:::::aaaa::::::a      tt::::::::::::::t  GG:::::::::::::::GP::::::::P                T:::::::::T                         
+                         CCC::::::::::::C h:::::h     h:::::h a::::::::::aa:::a       tt:::::::::::tt    GGG::::::GGG:::GP::::::::P                T:::::::::T                         
+                            CCCCCCCCCCCCC hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaa         ttttttttttt         GGGGGG   GGGGPPPPPPPPPP                TTTTTTTTTTT                         
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+*/
 
 #endif // UTILITIES_H_INCLUDED
